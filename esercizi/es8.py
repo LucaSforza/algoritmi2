ce8935579de26ae0d10f6af77379400ccd33d371
LucaSforza <lucasforza1234@icloud.com>
2024-03-05 16:43:30 +0100

    cambio nome funzione

--- esercizi/es8.py
+++ esercizi/es8.py
@@ -6,18 +6,18 @@ def init_completo(n: int) -> list[list[int]]:
                 res[y].append(i)
     return res
 
-def DFS(u: int, L: list[list[int]], V: list[int]):
+def es8(u: int, L: list[list[int]], V: list[int]):
     V[u] = 1
     for x in reversed(L[u]):
         if V[x] == 0:
             L[x].pop()
     if u != 0:
-        DFS(u-1,L,V)
+        es8(u-1,L,V)
             
     
 if __name__ == '__main__':
     n = 3
     V = [0 for _ in range(n)]
     L = init_completo(n)
-    DFS(n - 1,L,V)
+    es8(n - 1,L,V)
     print(L)
