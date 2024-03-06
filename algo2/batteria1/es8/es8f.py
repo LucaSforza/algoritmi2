0bdf378f2fafbf396ff1a11653ed7b9cef48ad80
filp8 <filippo.onofri46@gmail.com>
2024-03-06 23:47:22 +0100

    finito esercizio 8R dopo vari smadonnamenti

--- algo2/batteria1/es8/es8f.py
+++ algo2/batteria1/es8/es8f.py
@@ -1,5 +1,5 @@
 grafo1 = [[1],[3],[1,4],[4],[2,3],[4]]
-grafo2 = [[1],[2],[3],[0]]
+grafo2 = [[1,3],[2],[3],[0]]  # expected [[1, 3], [2], [3], []]
 visitati = [0]*len(grafo2)
 def es82f(grafo:list[list[int]],nodo:int):
     if visitati[nodo]==1:
@@ -8,15 +8,14 @@ def es82f(grafo:list[list[int]],nodo:int):
         visitati[nodo]=1
         for arco in grafo[nodo]:
             if nodo in grafo[arco] and arco in grafo[nodo]:
-                if arco in visitati:
-                    grafo[arco].remove(nodo)
+                if visitati[arco]==1:
+                    grafo[nodo].remove(arco)
                 else:
                     grafo[arco].remove(nodo)
             else:
                 es82f(grafo,arco)
 
-es82f(grafo2,0) 
-print(grafo2)
+
 
 
 
