040d892e22bb3153fb2941b2f68fbb099262536a
filp8 <filippo.onofri46@gmail.com>
2024-03-06 16:58:12 +0100

    es 8 lasciato a meta

--- algo2/batteria1/es8/es8f.py
+++ algo2/batteria1/es8/es8f.py
@@ -1,15 +1,22 @@
-grafo = [[1],[3],[1,4],[4],[2,3],[4]]
-visitati = [0]*len(grafo)
-def es82f(grafo:list[list[int]],nodo:int)->bool:
-    if nodo in visitati:
-        return False
+grafo1 = [[1],[3],[1,4],[4],[2,3],[4]]
+grafo2 = [[1],[2],[3],[0]]
+visitati = [0]*len(grafo2)
+def es82f(grafo:list[list[int]],nodo:int):
+    if visitati[nodo]==1:
+        return 
     else:
         visitati[nodo]=1
         for arco in grafo[nodo]:
-            if nodo in grafo[arco]:
-                
+            if nodo in grafo[arco] and arco in grafo[nodo]:
+                if arco in visitati:
+                    grafo[arco].remove(nodo)
+                else:
+                    grafo[arco].remove(nodo)
             else:
                 es82f(grafo,arco)
+
+es82f(grafo2,0) 
+print(grafo2)
 
 
 
