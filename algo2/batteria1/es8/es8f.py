bacbd79fea3deab08e012c821a0755e61e2a13ea
filp8 <filippo.onofri46@gmail.com>
2024-03-07 01:39:48 +0100

    fine

--- algo2/batteria1/es8/es8f.py
+++ algo2/batteria1/es8/es8f.py
@@ -1,3 +1,23 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+G = nx.DiGraph()
+G.add_edges_from([("A","B")],("A","C"),("C","B"))
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G,pos,node_size=500)
+nx.draw_networkx_edges(G,pos,edgelist=G.edges(),edge_color='black')
+nx.draw_networkx_labels(G,pos)
+plt.show()
+
+
+
+
+
+
+
+
+
 grafo1 = [[1],[3],[1,4],[4],[2,3],[4]]
 grafo2 = [[1,3],[2],[3],[0]]  # expected [[1, 3], [2], [3], []]
 visitati = [0]*len(grafo2)
@@ -14,6 +34,7 @@ def es82f(grafo:list[list[int]],nodo:int):
                     grafo[arco].remove(nodo)
             else:
                 es82f(grafo,arco)
+
 
 
 
