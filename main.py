c4b4fa89bb23bafb45a1d75a44f16172d11d5549
filp8 <filippo.onofri46@gmail.com>
2024-03-10 05:09:15 +0100

    aggiunto file main

---
+++ main.py
@@ -0,0 +1,12 @@
+from draw.draw import *
+from algo2.batteria1.es7.es7f import *
+
+
+
+print(es7f(netGrafToListAdiacenzaList(loadGraphFile("grafo_esempio_es7_networkx")),))
+
+# netGr = edgeListToNetGraf([("A","B"),("A","C"),("A","D"),("C","D")])
+# drawNetGraf(netGr)
+# adiList = netGrafToListAdiacenzaList(netGr)
+
+#es7f(adiList)
