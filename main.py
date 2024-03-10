88040de61778513a043c4d5bdae0672c7a89cf9d
filp8 <filippo.onofri46@gmail.com>
2024-03-10 05:45:32 +0100

    es 7 funziona anche con secondo input di prova

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from draw.draw import *
 from algo2.batteria1.es7.es7f import *
 
 
-
-#print(es7f(netGrafToListAdiacenzaList(loadGraphFile("grafo_esempio_es7_networkx")),))
 '''
 per inserire un grafo nella cartella dei gafi salvati onserendolo in input da terminale
 '''
@@ -19,13 +17,6 @@ disegnare un grafo da uno salvato su un file
 '''
 provare un esercizio usando lista di adiacenza
 '''
-netgraf = loadGraphFile("grafo_prova_es7_3_networkx")
-listaAdiacenza = netGrafToListAdiacenzaList(netgraf)
-print(es7f(listaAdiacenza))
-
-
-# netGr = edgeListToNetGraf([("A","B"),("A","C"),("A","D"),("C","D")])
-# drawNetGraf(netGr)
-# adiList = netGrafToListAdiacenzaList(netGr)
-
-#es7f(adiList)
+# netgraf = loadGraphFile("grafo_prova_es7_3_networkx")
+# listaAdiacenza = netGrafToListAdiacenzaList(netgraf)
+# print(es7f(listaAdiacenza))
