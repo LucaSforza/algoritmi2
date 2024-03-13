6c2ae4cb3f32d9e22d2aa7c3c7d74c97cb42ef4b
filp8 <filippo.onofri46@gmail.com>
2024-03-13 22:57:55 +0100

    appunti algo 2 13 marzo

---
+++ algo2/essett6.py
@@ -0,0 +1,14 @@
+
+def DFS(grafo,nodo):
+
+    return
+def contaArchi(grafo:list[list[int]],nodo:int):
+    visitati = [0]*len(grafo)
+    DFS()
+    return
+
+
+if __name__=="__main__":
+    graf = [[1,2],[3],[3],[4],[5],[6],[1]]
+
+    contaArchi(graf,0)
