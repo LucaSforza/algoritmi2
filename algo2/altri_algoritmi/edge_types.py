e0cccdb2e4f084037f22563a841a96323075968a
LucaSforza <lucasforza1234@icloud.com>
2024-03-27 16:24:52 +0100

    algoritmo per contare archi in avanti, all'indietro e di attraversamento

---
+++ algo2/altri_algoritmi/edge_types.py
@@ -0,0 +1,41 @@
+from draw import draw
+
+G = [
+    [1,2,5],
+    [4],
+    [3],
+    [0,7],
+    [5,6,7],
+    [1,6],
+    [],
+    [6],
+]
+
+# ritorna in pos 0 archi in avanti, pos 1 archi all'indietro e pos 2 archi di attraversamento
+def count_edges(AL: list[list[int]]) -> tuple[int,int,int]:
+    res = [0,0,0]
+    V = [0 for _ in range(len(AL))] # O(n)
+    time = [0]
+    timestamp = [-1 for _ in range(len(AL))] # O(n)
+    DFS1(0,AL,V,res,time,timestamp) # O(n + m)
+    return res[0],res[1],res[2]
+
+def DFS1(u: int, L: list[list[int]], V: list[int], res: list[int], time: list[int], timestamp: list[int]) -> None:
+    V[u] = 1
+    timestamp[u] = time[0]
+    time[0] += 1
+    for v in L[u]:
+        if V[v] == 0:
+            DFS1(v,L,V,res, time, timestamp)
+        elif V[v] == 1:
+            res[1] += 1
+        else:
+            if timestamp[u] > timestamp[v]:
+                res[2] += 1
+            else:
+                res[0] += 1
+    V[u] = 2
+
+if __name__ == '__main__':
+    print(count_edges(G))
+    #draw(G,direct=True)
