becfa2ef3f1d212ee14205959c81e93de84a0754
filp8 <filippo.onofri46@gmail.com>
2024-04-08 16:02:10 +0200

    provatoa a sistemare mergesort

--- algo1/algoritmiOrdinamento.py
+++ algo1/algoritmiOrdinamento.py
@@ -50,5 +50,65 @@ def bubleSort(A:list[int])->list[int]:
     return
 
 '''
+merge Sort
 
 '''
+def mergeSortIter(lista,a,b):
+    n = len(lista)
+    l = 1
+    while l<n:
+        i = 0
+        while i-l<n:
+
+            fondi(lista,i,i+l-1,i+2*l-1)
+            i+=2*l
+        l*=2
+
+def mergeSortR(lista,i,j):
+    if i<j:
+        m = (i+j)//2
+        mergeSortR(lista,i,m)
+        mergeSortR(lista,m+1,j)
+        out = fondi(lista,i,m,j)
+        return out
+
+def fondi(lista,a,m,b):
+    i = a
+    j = m+1
+    listOut = []
+    while i<=m and j<=b:
+        if lista[i]<=lista[j]:
+            listOut.append(lista[i])
+            i+=1
+        else:
+            listOut.append(lista[j])
+            j+=1
+    return listOut
+
+l1 = [2,4,6]
+l2 = [1,3,5,7]
+print(fondi(l1,l2))
+# da sistemare
+
+# esempio1 = [9,1,8,2,7,3,6,4,5]
+# print(mergeSortR(esempio1,0,len(esempio1)-1))
+
+
+'''
+Quick Sort
+'''
+
+def quickSort(lista,a,b):
+    S,C,D = [],[],[]
+    if len(lista)==0:
+        return lista
+    p=lista[a]
+    for i in range(len(lista)):
+        if lista[i]<p: S.append(lista[i])
+        elif lista[i]==p: C.append(lista[i])
+        else: D.append(lista[i])
+    return quickSort(S,0,len(S)-1)+C+quickSort(D,0,len(D)-1)
+
+# esempio1 = [9,1,8,2,7,3,6,4,5]
+# print(quickSort(esempio1,0,len(esempio1)-1))
+
---
+++ algo1/mergeSort.py
@@ -0,0 +1,3 @@
+
+# il merge sort ha tempo O(n log n) che e il miglior tempo che possiamo ottenere per un algoritmo di ordinaeto 
+
