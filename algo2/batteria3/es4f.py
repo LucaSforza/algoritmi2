8db6b5382d235b5909d69cec775301a81d6ed7ac
filp8 <filippo.onofri46@gmail.com>
2024-04-08 03:21:07 +0200

    es4 batteria 3 implementato

--- algo2/batteria3/es4f.py
+++ algo2/batteria3/es4f.py
@@ -1,23 +1,30 @@
 from draw import draw
 
-def dfs(grafo:list[list[int]],currNodo:int,padre:int,visitati:list[int],grafOut:list[list[int]]):
+def dfs(grafo:list[list[int]],currNodo:int,padre:int,visitati:list[int],grafOut:list[set[int]],finita):
     visitati[currNodo]=1
     for adiNodo in grafo[currNodo]:
         if visitati[adiNodo]==0:
-            dfs(grafo,adiNodo,currNodo,visitati,grafOut)
+            grafOut[currNodo].add(adiNodo)
+            dfs(grafo,adiNodo,currNodo,visitati,grafOut,finita)
+            finita[adiNodo]=True
         elif visitati[adiNodo]==1:
-            grafOut[currNodo].append(adiNodo)
+            if not finita[adiNodo]:
+                grafOut[adiNodo].add(currNodo)
+            
     return 
 
 
 def es4f(grafo:list[list[int]]):
     visitati = [0]*len(grafo)
-    grafOut = [[] for _ in range(len(grafo))]
-    dfs(grafo,0,0,visitati,grafOut)
+    grafOut = [set() for _ in range(len(grafo))]
+    finita = [False for _ in range(len(grafo))]
+    dfs(grafo,0,0,visitati,grafOut,finita)
+    return [list(l) for l in grafOut]
 
 
 
 
-esempio=[[3,4],[2,3,4,5],[1,4,5],[0,1],[0,1,2],[1,2]]
-
-draw(esempio)
+esempio = [[2,3],[4,5],[0,3,4,5],[0,2,5],[1,2],[1,2,3]]
+print(es4f(esempio))
+draw(es4f(esempio),direct = True)
+#draw(esempio)
