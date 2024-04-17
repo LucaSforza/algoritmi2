a0c6a1fc020b51954dd35ba4948e8063298fa0c7
filp8 <filippo.onofri46@gmail.com>
2024-04-18 01:09:43 +0200

    sottostringhe somma massima

--- algo2/batteria3/es5f.py
+++ algo2/batteria3/es5f.py
@@ -1,24 +1,101 @@
+from draw import draw
 
 
-def tarjan(grafo,nodo,visitati,stack,minDist)->int:
-    visitati[nodo]=1
-    minDist[nodo]=nodo
+def dfs(grafo,nodo,ids,low,stack,onStack,id):
+    scc = 0
     stack.append(nodo)
-    for adiNodo in grafo[nodo]:
-        if visitati[adiNodo]==0:
-            tarjan(grafo,adiNodo,visitati,stack,minDist)
-            minDist[adiNodo]=min(minDist[nodo],minDist[adiNodo])
-        elif adiNodo in stack:
-            minDist[adiNodo]=min(minDist[nodo],minDist[adiNodo])
-        
-    return 
+    onStack[nodo]=True
+    ids[nodo]=id
+    low[nodo]=id
+    id +=1
+    for adi_nodo in grafo[nodo]:
+        if ids[adi_nodo]==-1:
+            scc+=dfs(grafo,adi_nodo,ids,low,stack,onStack,id)
+        if onStack[adi_nodo]:
+            low[nodo] = min(low[nodo],low[adi_nodo])
+    if ids[nodo]==low[nodo]:
+        for _ in range(len(grafo)):
+            node = stack.pop()
+            onStack[node] = False
+            low[node]=ids[nodo]
+            if node == nodo: break
+        return scc+1
+    return scc
+    
 
-def es5f(grafo)->list[list[int]]:
-    indice = 0 
+def findScc(grafo,ids,low,stack,onStack,id):
+    nSCC = 0
+    for i in range(len(grafo)):
+        if ids[i]==-1:
+            nSCC+=dfs(grafo,i,ids,low,stack,onStack,id)
+    
+    return low
+
+def es5(grafo):
+    n = len(grafo)
+    id = 0
+    ids = [-1]*n
+    low = [0]*n
+    onStack = [False for _ in range(n)] 
     stack = []
-    nNodi = len(grafo)
-    minDist = [nNodi+1]*nNodi
-    visitati = [0]*nNodi
-    for nodo in range(len(grafo)):
-        if visitati[nodo]==0:
-            tarjan(grafo,nodo,visitati,stack,minDist)
+    low = findScc(grafo,ids,low,stack,onStack,id)
+    trad = dict()
+    low_format = []
+    ncomp = 0
+
+    for comp in low:
+        if comp in trad:
+            low_format.append(trad[comp])
+        else:
+            trad[comp]=ncomp
+            low_format.append(ncomp)
+            ncomp+=1
+
+    graf_conds = [[] for _ in range(ncomp)]
+    for node,comp in enumerate(low_format):
+        for edge in grafo[node]:
+            if low_format[edge]!=comp:
+                graf_conds[comp].append(low_format[edge])
+
+    return graf_conds
+            
+
+    
+
+
+
+
+
+
+
+
+
+esempio1 = [[16], #0
+            [0,2],#1
+            [8],#2
+            [7],#3
+            [3,5,6],#4
+            [7],#5
+            [5,11],#6
+            [4,9,13],#7
+            [0],#8
+            [14],#9
+            [],#10
+            [12],#11
+            [11],#12
+            [10],#13
+            [15,17],#14
+            [9],#15
+            [1,4],#16
+            [9]#17
+            ]
+
+esempio2 = [[1],[2,3],[0],[4],[5],[3]]
+esempio3 = [[1],[]]
+esempio4 = [[1],[2],[1]]
+esempio5 = [[1],[0],[0]]
+esempio6 = [[1],[2],[3,4],[1],[1]]
+
+
+draw(es5(esempio4),direct=True)
+#draw(esempio1,direct = True)
