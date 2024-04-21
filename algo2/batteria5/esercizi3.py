b6b80107d0c45c89296909420a6b660a1ba78046
LucaSforza <lucasforza1234@icloud.com>
2024-04-21 09:46:41 +0200

    esercizio 3

---
+++ algo2/batteria5/esercizi3.py
@@ -0,0 +1,15 @@
+
+def es(V,i,j):
+    if i == j: return V[i] == i
+    m = (i+j)//2
+    if V[m] == m: return True
+    if V[m] > m:
+        return es(V,i,m)
+    else:
+        return es(V,i,m)
+
+if __name__ == '__main__':
+    V = [5,6,7,8]
+    print(es(V,0,len(V)-1))
+    V = [0,2,3]
+    print(es(V,0,len(V)-1))
